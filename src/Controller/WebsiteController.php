0751d4cca0faadb575aecf398c815d37505f5630
Adrian Florczak <kontakt@adrianflorczak.pl>
2023-06-06 17:18:31 +0200

    faq, organization, branch, privacyPolicy, rodo pages

--- src/Controller/WebsiteController.php
+++ src/Controller/WebsiteController.php
@@ -13,10 +13,40 @@ class WebsiteController extends AbstractController
         return $this->render('website/view/home.html.twig');
     }
 
-    #[Route('/contact', name: 'app_website_contact', methods: ['GET'])]
-    public function contact(): Response {
-        return $this->render('website/view/contact.html.twig', [
-            'title' => 'Kontakt'
+    #[Route('/{organization}', name: 'app_website_organization', methods: ['GET'])]
+    public function organization(string $organization): Response {
+        return $this->render('website/view/organization/index.html.twig', [
+            'organization' => $organization
         ]);
+    }
+
+    #[Route('/{organization}/{branch}', name: 'app_website_organization_branch', methods: ['GET'])]
+    public function branch(string $organization, string $branch): Response {
+        return $this->render('website/view/organization/branch/index.html.twig', [
+            'organization' => $organization,
+            'branch' => $branch
+        ]);
+    }
+
+    #[Route('/faq', name: 'app_website_faq', methods: ['GET'], priority: 10)]
+    public function faq(): Response {
+        return $this->render('website/view/faq.html.twig', []);
+    }
+
+    #[Route('/rodo', name: 'app_website_rodo', methods: ['GET'], priority: 10)]
+    public function rodo(): Response {
+        return $this->render('website/view/rodo.html.twig', []);
+    }
+
+
+
+    #[Route('/privacy-policy', name: 'app_website_privacy-policy', methods: ['GET'], priority: 10)]
+    public function privacyPolicy(): Response {
+        return $this->render('website/view/privacyPolicy.html.twig', []);
+    }
+
+    #[Route('/contact', name: 'app_website_contact', methods: ['GET'], priority: 10)]
+    public function contact(): Response {
+        return $this->render('website/view/contact.html.twig', []);
     }
 }
