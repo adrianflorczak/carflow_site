0bd4969caa0658e03c2b30423415938b4c20d140
Adrian Florczak <kontakt@adrianflorczak.pl>
2023-06-08 13:55:36 +0200

    Basic Bootstrap@3 in website

--- assets/website/global.js
+++ assets/website/global.js
@@ -1,2 +1,9 @@
-import 'normalize.css';
+// import 'normalize.css';
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './global.css';
+
+$(document).ready(function() {
+
+});
