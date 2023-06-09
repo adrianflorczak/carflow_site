ef6d811baa8f8cf8d2bfe589be0500c6dd6d214c
Adrian Florczak <kontakt@adrianflorczak.pl>
2023-06-09 09:16:48 +0200

    Functionalities Views in Website

--- src/Controller/Website/FunctionalitiesController.php
+++ src/Controller/Website/FunctionalitiesController.php
@@ -11,6 +11,21 @@ class FunctionalitiesController extends AbstractController
 {
     #[Route('', name: 'app_website_functionalities', methods: ['GET'], priority: 10)]
     public function functionalities(): Response {
-        return $this->render('website/view/functionalities/index.html.twig');
+        return $this->render('website/view/functionalities/home/index.html.twig');
+    }
+
+    #[Route('/for-client', name: 'app_website_functionalities_for-client', methods: ['GET'], priority: 10)]
+    public function functionalitiesForClient(): Response {
+        return $this->render('website/view/functionalities/forClient/index.html.twig');
+    }
+
+    #[Route('/for-car-flow-site-rentals', name: 'app_website_functionalities_for-car-flow-site-rentals', methods: ['GET'], priority: 10)]
+    public function functionalitiesForCarFlowSiteRentals(): Response {
+        return $this->render('website/view/functionalities/forCarFlowSiteRentals/index.html.twig');
+    }
+
+    #[Route('/for-external-rentals', name: 'app_website_functionalities_for-external-rentals', methods: ['GET'], priority: 10)]
+    public function functionalitiesForExternalRentals(): Response {
+        return $this->render('website/view/functionalities/forExternalRentals/index.html.twig');
     }
 }
