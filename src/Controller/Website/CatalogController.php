dab3ac67ca753604ad8cf75cac3b9da5cf898100
Adrian Florczak <kontakt@adrianflorczak.pl>
2023-06-09 12:24:38 +0200

    Catalog Organizations Page

--- src/Controller/Website/CatalogController.php
+++ src/Controller/Website/CatalogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Website;
 
+use App\Service\OrganizationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/catalog')]
 class CatalogController extends AbstractController
 {
+    private OrganizationService $organizationService;
+
+    public function __construct(
+        OrganizationService $organizationService
+    )
+    {
+        $this->organizationService = $organizationService;
+    }
+
     #[Route('/organizations', name: 'app_website_catalog_organizations', methods: ['GET'], priority: 10)]
-    public function organizations(): Response {
-        return $this->render('website/view/catalog/organizations/index.html.twig', []);
+    public function getOrganizations(): Response {
+        $organizations = $this->organizationService->getAllOrganizations();
+
+        return $this->render('website/view/catalog/organizations/index.html.twig', [
+            'organizations' => $organizations
+        ]);
     }
 
     #[Route('/branches', name: 'app_website_catalog_branches', methods: ['GET'], priority: 10)]
