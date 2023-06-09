f2c0a24394a0e083d4773adf33bcc415bb2f637c
Adrian Florczak <kontakt@adrianflorczak.pl>
2023-06-09 09:25:17 +0200

    PriceList Views in Website

--- src/Controller/Website/PriceListController.php
+++ src/Controller/Website/PriceListController.php
@@ -20,8 +20,27 @@ class PriceListController extends AbstractController
     #[Route('', name: 'app_website_price-list', methods: ['GET'], priority: 10)]
     public function priceList(): Response {
         $items = $this->service->getAllItems();
-        return $this->render('website/view/priceList/index.html.twig', [
+        return $this->render('website/view/priceList/home/index.html.twig', [
             'items' => $items
         ]);
+    }
+
+    #[Route('/for-client', name: 'app_website_price-list_for-client', methods: ['GET'], priority: 10)]
+    public function priceListForClient(): Response {
+        $items = $this->service->getAllItems();
+        return $this->render('website/view/priceList/forClient/index.html.twig', []);
+    }
+
+    #[Route('/for-car-flow-site-rentals', name: 'app_website_price-list_for-car-flow-site-rentals', methods: ['GET'], priority: 10)]
+    public function priceListForCarFlowSiteRentals(): Response {
+        $items = $this->service->getAllItems();
+        return $this->render('website/view/priceList/forCarFlowSiteRentals/index.html.twig', []);
+    }
+
+    
+    #[Route('/for-external-rentals', name: 'app_website_price-list_for-external-rentals', methods: ['GET'], priority: 10)]
+    public function priceListForExternalRentals(): Response {
+        $items = $this->service->getAllItems();
+        return $this->render('website/view/priceList/forExternalRentals/index.html.twig', []);
     }
 }
